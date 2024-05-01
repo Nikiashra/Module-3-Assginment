14df02212204223d140b435f53486406804b88e1
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2024-05-02 00:52:08 +0530

    Add files via upload

---
+++ a/asg60/main.dart
@@ -0,0 +1,24 @@
+import 'package:asg_60/restaurant_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Restaurant App',
+      theme: ThemeData(
+        primarySwatch:Colors.pink,
+      ),
+      home:RestaurantPage(),
+
+    );
+  }
+}
+
